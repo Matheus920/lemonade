13457c0c63a092ccc1351af303b5437df61cb98c
Matheus Moreira <matheusm920@gmail.com>
2018-04-19 18:46:55 -0300

    Second commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import {MDCRipple} from '@material/ripple';
 import {MDCToolbar} from '@material/toolbar';
+import {MDCCheckbox} from '@material/checkbox';
+import {MDCSelect} from '@material/select';
 
 var buttons1 = [];
 
@@ -9,3 +11,12 @@ for(var j = 0; j < document.querySelectorAll('.mdc-button').length; j++){
 
 var toolbar = MDCToolbar.attachTo(document.querySelector('.mdc-toolbar'));
 toolbar.fixedAdjustElement = document.querySelector('.mdc-toolbar-fixed-adjust');
+
+var checkboxs = [];
+
+for(var i = 0; i < document.querySelectorAll('.mdc-checkbox').length; i++){
+    checkboxs[i] = new MDCCheckbox(document.querySelectorAll('.mdc-checkbox')[i]);
+}
+
+const select = new MDCSelect(document.querySelector('.mdc-select'));
+console.log(select);

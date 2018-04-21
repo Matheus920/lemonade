0e17363472b395b3b76ebf59db2e2da7d50918d2
Matheus Moreira <matheusm920@gmail.com>
2018-04-21 17:40:42 -0300

    Arrow buttons now work

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-import {MDCRipple} from '@material/ripple';
-import {MDCToolbar} from '@material/toolbar';
-import {MDCCheckbox} from '@material/checkbox';
-import {MDCSelect} from '@material/select';
-import {mui} from 'muicss/dist/js/mui';
-
+import { MDCRipple } from '@material/ripple';
+import { MDCToolbar } from '@material/toolbar';
+import { MDCCheckbox } from '@material/checkbox';
+import { MDCSelect } from '@material/select';
+import { mui } from 'muicss/dist/js/mui';
 var buttons1 = [];
 
-for(var j = 0; j < document.querySelectorAll('.mdc-button').length; j++){
+for (var j = 0; j < document.querySelectorAll('.mdc-button').length; j++) {
     buttons1[j] = new MDCRipple(document.querySelectorAll('.mdc-button')[j]);
 }
 
@@ -15,20 +14,21 @@ toolbar.fixedAdjustElement = document.querySelector('.mdc-toolbar-fixed-adjust')
 
 var checkboxs = [];
 
-for(var i = 0; i < document.querySelectorAll('.mdc-checkbox').length; i++){
+for (var i = 0; i < document.querySelectorAll('.mdc-checkbox').length; i++) {
     checkboxs[i] = new MDCCheckbox(document.querySelectorAll('.mdc-checkbox')[i]);
 }
 
 const select = new MDCSelect(document.querySelector('.mdc-select'));
 console.log(select);
 
-b.click(function() {
-    if(!b.is('glayson')){
-    console.log('glayson existe')
-    b.attr('glayson', 'rola')
-    setTimeout(() => $('div.mui-select__menu'), 300)
-    }else{
-    b.prop('glayson', null)
-    console.log('glayson foi removido')
+b.click(function () {
+    if (!b.is('glayson')) {
+        console.log('glayson existe')
+        b.attr('glayson', 'rola')
+        setTimeout(() => $('div.mui-select__menu'), 300)
+    } else {
+        b.prop('glayson', null)
+        console.log('glayson foi removido')
     }
-    })
+});
+

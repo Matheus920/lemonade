b2920644e4ba5defb56358cf8aa72dd1d0128345
Matheus Moreira <matheusm920@gmail.com>
2018-05-10 18:29:03 -0300

    Worker page init

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import { MDCTextField } from '@material/textfield';
 import { MDCDialog, MDCDialogFoundation, util } from '@material/dialog';
 import { MDCRadio } from '@material/radio';
 
+
 var radios = [];
 
 for (var z = 0; z < document.querySelectorAll('.mdc-radio').length; z++) {
@@ -27,13 +28,74 @@ var checkboxs = [];
 
 for (var i = 0; i < document.querySelectorAll('.mdc-checkbox').length; i++) {
     checkboxs[i] = new MDCCheckbox(document.querySelectorAll('.mdc-checkbox')[i]);
+    console.log(checkboxs[i]);
 }
+
+
+
 
 var textfields = [];
 
 for (var k = 0; k < document.querySelectorAll('.mdc-text-field').length; k++) {
     textfields[i] = new MDCTextField(document.querySelectorAll('.mdc-text-field')[k]);
 }
+
+var dialog = new MDCDialog(document.querySelector('#evaluation-dialog'));
+var dialog2 = new MDCDialog(document.querySelector('#special-evaluation-dialog'));
+var dialog3 = new MDCDialog(document.querySelector('#feedback-dialog'));
+
+
+dialog.listen('MDCDialog:accept', function () {
+    console.log('accepted');
+});
+
+dialog.listen('MDCDialog:cancel', function () {
+    console.log('canceled');
+});
+
+dialog2.listen('MDCDialog:accept', function () {
+    console.log('accepted');
+});
+
+dialog2.listen('MDCDialog:cancel', function () {
+    console.log('canceled');
+});
+
+dialog3.listen('MDCDialog:accept', function () {
+    console.log('accepted');
+});
+
+dialog3.listen('MDCDialog:cancel', function () {
+    console.log('canceled');
+});
+
+for (var le = 0; le < document.querySelectorAll('.button-confirm').length; le++) {
+    document.querySelectorAll('.button-confirm')[le].onclick = function (evt) {
+        if (this.innerHTML === 'Feedback') {
+            dialog3.lastFocusedTarget = evt.target;
+            dialog3.show();
+        } else {
+            if (this.parentElement.parentElement.children[4].innerHTML === 'Avulsa') {
+                dialog.lastFocusedTarget = evt.target;
+                dialog.show();
+            } else {
+                dialog2.lastFocusedTarget = evt.target;
+                dialog2.show();
+            }
+        }
+    }
+}
+
+document.querySelector('#problema_id').onclick = function () {
+    if (checkboxs[0].checked) {
+        console.log('teste');
+        document.querySelector('#description-problem').innerHTML = '<span class="mdc-typography--subtitle">Tipo de problema</span><section><label id="ocorrencia-radio-label-id"><div class="mdc-radio"><input class="mdc-radio__native-control" type="radio" id="ocorrencia-radio-id" name="reserve-type" checked><div class="mdc-radio__background"><div class="mdc-radio__outer-circle"></div><div class="mdc-radio__inner-circle"></div></div></div>Ocorrência</label><label id="anormalidade-radio-label-id"><div class="mdc-radio"><input class="mdc-radio__native-control" type="radio" id="anormalidade-radio-id" name="reserve-type"><div class="mdc-radio__background"><div class="mdc-radio__outer-circle"></div><div class="mdc-radio__inner-circle"></div></div></div>Anormalidade</label></section><div class="row"><form class="col s12"><div class="row"><div class="input-field col s12"><textarea id="feedback-textarea-id" class="materialize-textarea"></textarea><label for="feedback-textarea-id">Dê mais informações sobre o problema</label></div></div></form></div>';
+    } else {
+        console.log('teste1');
+        document.querySelector('#description-problem').textContent = 'Caso não haja nenhum problema, clique em confirmar.';
+    }
+}
+
 
 const select = new MDCSelect(document.querySelector('.mdc-select'));
 console.log(select);
